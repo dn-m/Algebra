79a76c7071b7ec11bc151b8bf2c007bd5788ddb1
James <jbean46@gmail.com>
2017-07-24 22:31:27 +0200

    Tidy up fake test

--- Tests/AlgebraTests/AlgebraTests.swift
+++ Tests/AlgebraTests/AlgebraTests.swift
@@ -2,12 +2,10 @@ import XCTest
 @testable import Algebra
 
 class AlgebraTests: XCTestCase {
+
     func testOneIsOne() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
         XCTAssertEqual(1,1)
     }
-
 
     static var allTests = [
         ("testOneIsOne", testOneIsOne),
